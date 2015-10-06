152030e1dc94cb2f42c6c33f94909bc47bd61439
andela-sakande <surajudeen.akande@andela.com>
2015-10-06 16:28:36 +0100

    add Exception Comment

--- src/Exceptions/NullUserException.php
+++ src/Exceptions/NullUserException.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * This exception package handles empty username.
+ *
+ * @package Open Source Evangelist Agnostic Package
+ *@author Surajudeen AKANDE <surajudeen.akande@andela.com>
+ *@license MIT <https://opensource.org/licenses/MIT>
+ *@link http://www.github.com/andela-sakande
+ */
 
 namespace Sirolad\Evangelist\Exceptions;
 
 use Exception;
 
+/*
+This class returns an exception message for empty username.
+ */
 class NullUserException extends Exception
 {
     public function message()
