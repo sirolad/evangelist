299a30dc122ea1b60ef29de27f504c3560effeed
andela-sakande <surajudeen.akande@andela.com>
2015-10-06 16:25:35 +0100

    add Evangelist Ranker Comment

--- src/EvangelistRanker.php
+++ src/EvangelistRanker.php
@@ -1,12 +1,33 @@
 <?php
+/**
+ * This package fetches the data of requested individual using the Github API and ranks the Individual into
+ * categories based on the number of public repositories the individual possesses.
+ *@package Open Source Evangelist Agnostic Package
+ *@author Surajudeen AKANDE <surajudeen.akande@andela.com>
+ *@license MIT <https://opensource.org/licenses/MIT>
+ *@link http://www.github.com/andela-sakande
+ **/
 namespace Sirolad\Evangelist;
 
 use Sirolad\Evangelist\EvangelistFetcher;
 use GuzzleHttp\Exception\ClientException;
 use Sirolad\Evangelist\Exceptions\NullUserException;
 
+/*
+EvangelistRanker is the main class that perfoms the logic of ranking requested repository.
+ */
+
 class EvangelistRanker
 {
+    /**
+     * Returns the rank of requested repository into Senior, Associate and Junior Evangelists
+     *
+     * @param  string $username Supply the username searched for on GitHub
+     * @return string
+     * @throws NullUserException
+     * @throws ClientException
+     *
+     */
     public function rankEvangelist($username)
     {
         try {
